d3193672b6c9a7fd8c68cca931d34b31c2b50a87
dhruv patel <dhruvjpatel5@gmail.com>
2025-05-07 21:58:06 +0530

    Add JWT authentication and user registration features; update user model and routes

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,37 +1,76 @@
-const User = require('../models/user-model');
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
+
 //home page//
 
-const home = async(req,res)=>{
+const home = async (req, res) => {
+  try {
+    res.status(200).send("welcome to home page");
+  } catch (error) {
+    console.log(error);
+  }
+};
+//register page
+const register = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, email, phone, password } = req.body;
+
+    const userExiat = await User.findOne({ email: email });
+    if (userExiat) {
+      return res.status(400).json({ message: "User email already exists" });
+    }
+
+    // //hash the password
+    // const saltRound = 10;
+    // const hash_password = await bcrypt.hash(password, saltRound)
+
+    const userCreate = await User.create({
+      username,
+      email,
+      phone,
+      password,
+    });
+    res
+      .status(201)
+      .json({
+        message: "User registration successfully",
+        token: await userCreate.generateToken(),
+        userId: userCreate._id.toString(),
+      });
+    const data = await User.find;
+
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+
+
+//login page
+
+const login = async (req,res)=>{
     try {
-        res.status(200).send('Login Page');
+        const {email,password} = req.body;
+        const userExist = await User.findOne({ email });
+        if(!userExist){
+            return res.status(400).json({message: "Invalid creadentiols"})
+        }
+        const user = await bcrypt.compare(password, userExist.password);
+        if(user){
+            return res.status(200).json({
+                message: "Login successfully",
+                token: await userExist.generateToken(),
+                userId: userExist._id.toString(),
+            })
+        }else{
+            return res.status(400).json({message: "Invalid email or password"})
+        }
+
     } catch (error) {
         console.log(error);
         
     }
 }
-//register page
-const register = async(req,res)=>{
-    try {
-         console.log(req.body)
-        const {username, email, phone, password} = req.body;
-
-        const userExiat = await User.findOne({email :email});
-        if(userExiat){
-            return res.status(400).json({message : "User email already exists"})
-        }
-        const userCreate = await User.create({
-            username,
-            email,
-            phone,
-            password,
-        });
-        res.status(201).json({message : userCreate})
-        const data = await User.find
-
-
-        res.status(200).json({data});
-    } catch (error) {
-        console.log(error);
-    }
-}
-module.exports = { home , register};
+module.exports = { home, register, login};
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -25,6 +28,37 @@ const userSchema = new mongoose.Schema({
     },
 })
 //define a models and the collection name 
+userSchema.pre('save',async function(next){
+   const user = this;
+   if(!user.isModified('password')) return next();
+   try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hash_password;
+   } catch (error) {
+       console.log(error);
+       next(error);
+    
+   }
+})
+
+userSchema.methods.generateToken = async function(){
+    try {
+        return jwt.sign({
+            userId: this._id.toString(),
+            email: this.email,
+            isAdmin: this.isAdmin,
+        },
+        process.env.JWT_SECRET_KEY, {
+            expiresIn: '30d',
+        });
+    
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
 
 const User  = new mongoose.model('User', userSchema);
 module.exports = User;
